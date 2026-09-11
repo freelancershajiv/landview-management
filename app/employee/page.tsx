def14045f86367ec611f25234324f677bb99b325
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-12 04:34:28 +0600

    Show employee certificate center in portal

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,5 +1,6 @@
 import EmployeeCommandCenter from "@/components/employee-command-center";
+import EmployeeCertificateCenter from "@/components/employee-certificate-center";
 
 export default function EmployeePortalPage() {
-  return <EmployeeCommandCenter />;
+  return <><EmployeeCommandCenter /><EmployeeCertificateCenter /></>;
 }
