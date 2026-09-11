9c822c6456229a8fe2d99490d587dc916ede8dfb
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-12 04:35:38 +0600

    Show client certificate center in portal

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -1,7 +1,8 @@
 import RolePortalShell from "@/components/role-portal-shell";
+import ClientCertificateCenter from "@/components/client-certificate-center";
 import { requirePortalSession } from "@/lib/server-auth";
 
 export default async function ClientLayout({ children }: { children: React.ReactNode }) {
   await requirePortalSession(["client"]);
-  return <RolePortalShell portal="client">{children}</RolePortalShell>;
+  return <RolePortalShell portal="client"><>{children}<ClientCertificateCenter /></></RolePortalShell>;
 }
