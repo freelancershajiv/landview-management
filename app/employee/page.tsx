e78abb7f68cf86a6a682c38c7be93d7fa786fe89
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-12 23:23:20 +0600

    Add office expense submission to employee portal

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,6 +1,7 @@
 import EmployeeCommandCenter from "@/components/employee-command-center";
 import EmployeeCertificateCenter from "@/components/employee-certificate-center";
+import EmployeeExpenseCenter from "@/components/employee-expense-center";
 
 export default function EmployeePortalPage() {
-  return <><EmployeeCommandCenter /><EmployeeCertificateCenter /></>;
+  return <><EmployeeCommandCenter /><EmployeeExpenseCenter /><EmployeeCertificateCenter /></>;
 }
