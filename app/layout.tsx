4b5ac17207729dc59523df0fa0c3553ffa433605
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-12 20:32:59 +0600

    Load unified top portal navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./app-brand-theme.css";
 import "./action-feedback.css";
 import "./portal-experiences.css";
 import "./brand-logo-overrides.css";
+import "./top-portal-navigation.css";
 
 import { siteUrl, businessSchema, jsonLd } from "@/lib/site-info";
 
