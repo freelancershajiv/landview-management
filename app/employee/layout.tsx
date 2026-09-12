ac949ab955772b0469534648f3768dcd4bc02221
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-12 13:27:10 +0600

    Preload employee workspace data in background

--- app/employee/layout.tsx
+++ app/employee/layout.tsx
@@ -1,7 +1,8 @@
+import PortalPreloader from "@/components/portal-preloader";
 import RolePortalShell from "@/components/role-portal-shell";
 import { requirePortalSession } from "@/lib/server-auth";
 
 export default async function EmployeeLayout({ children }: { children: React.ReactNode }) {
   await requirePortalSession(["employee"]);
-  return <RolePortalShell portal="employee">{children}</RolePortalShell>;
+  return <><PortalPreloader portal="employee" /><RolePortalShell portal="employee">{children}</RolePortalShell></>;
 }
