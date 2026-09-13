054d56276a68ff5e4f1178b576e517fa6488d3da
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-13 21:00:55 +0600

    Enable detailed client invoice generation

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -1,8 +1,9 @@
 import PortalPreloader from "@/components/portal-preloader";
 import RolePortalShell from "@/components/role-portal-shell";
+import ClientInvoiceLinkUpgrade from "@/components/client-invoice-link-upgrade";
 import { requirePortalSession } from "@/lib/server-auth";
 
 export default async function ClientLayout({ children }: { children: React.ReactNode }) {
   await requirePortalSession(["client"]);
-  return <><PortalPreloader portal="client" /><RolePortalShell portal="client">{children}</RolePortalShell></>;
+  return <><PortalPreloader portal="client" /><ClientInvoiceLinkUpgrade /><RolePortalShell portal="client">{children}</RolePortalShell></>;
 }
