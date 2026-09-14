462ab8a624bd88ba90e269d7bf1b38b82b08763c
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-14 19:01:06 +0600

    Load global typography system last

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./action-feedback.css";
 import "./portal-experiences.css";
 import "./brand-logo-overrides.css";
 import "./top-portal-navigation.css";
+import "./typography-system.css";
 
 import { siteUrl, businessSchema, jsonLd } from "@/lib/site-info";
 
