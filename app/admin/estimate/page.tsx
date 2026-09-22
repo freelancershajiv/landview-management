68e1b4905240a2ce12b399ec951aa3322edba319
freelancershajiv <49995006+freelancershajiv@users.noreply.github.com>
2026-09-23 00:19:10 +0600

    Use integrated live Summary Estimate

--- app/admin/estimate/page.tsx
+++ app/admin/estimate/page.tsx
@@ -1,11 +1,5 @@
 import DetailedEstimateWorkspace from "@/components/detailed-estimate-workspace";
-import SummaryEstimate from "@/components/summary-estimate";
 
 export default function EstimatePage(){
-  return (
-    <>
-      <DetailedEstimateWorkspace />
-      <SummaryEstimate />
-    </>
-  );
+  return <DetailedEstimateWorkspace />;
 }
